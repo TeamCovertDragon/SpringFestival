b43d7de10db9ed231b6268fb2c11992f78533ee9
Yesterday17 <t@yesterday17.cn>
2018-02-09 16:41:38 +0800

    Start working on FuDoor.

---
+++ src/main/java/team/covertdragon/springfestival/module/decoration/BlockFuDoor.java
@@ -0,0 +1,38 @@
+package team.covertdragon.springfestival.module.decoration;
+
+import net.minecraft.block.BlockDoor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+public class BlockFuDoor extends BlockDoor{
+
+    public BlockFuDoor(){
+        super(Material.WOOD);
+    }
+
+    @Override
+    public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
+    {
+        super.getDrops(drops, world, pos, state, fortune);
+
+        // TODO: Add Fu and the original Door here.
+    }
+
+    @Override
+    public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player)
+    {
+        return true;
+    }
+
+    @Override
+    protected ItemStack getSilkTouchDrop(IBlockState state)
+    {
+        // TODO: Add the item for the door in registry
+    }
+}
