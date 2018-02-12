3be396cf9f4767a232574c6aed495a3525f52485
Snownee <1850986885@qq.com>
2018-02-12 21:25:07 +0800

    Hanging firecracker physics

--- src/main/java/team/covertdragon/springfestival/module/firecracker/BlockHangingFirecracker.java
+++ src/main/java/team/covertdragon/springfestival/module/firecracker/BlockHangingFirecracker.java
@@ -9,20 +9,27 @@
 
 package team.covertdragon.springfestival.module.firecracker;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockFence;
+import net.minecraft.block.BlockGlass;
+import net.minecraft.block.BlockStainedGlass;
+import net.minecraft.block.BlockWall;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import team.covertdragon.springfestival.SpringFestivalConstants;
 import team.covertdragon.springfestival.internal.SpringFestivalUtil;
-
-import javax.annotation.Nullable;
 
 @SuppressWarnings("deprecation")
 public class BlockHangingFirecracker extends Block {
@@ -38,10 +45,37 @@ public class BlockHangingFirecracker extends Block {
     public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
         return NULL_AABB;
     }
+    
+    @Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
 
     @Override
-    public boolean canPlaceBlockOnSide(World worldIn, BlockPos pos, EnumFacing side) {
-        return side == EnumFacing.DOWN;
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+    
+    public boolean canPlaceBlockAt(World worldIn, BlockPos pos)
+    {
+        return super.canPlaceBlockAt(worldIn, pos) ? this.canBlockStay(worldIn, pos) : false;
+    }
+    
+    public boolean canBlockStay(World worldIn, BlockPos pos)
+    {
+        pos = pos.up();
+        IBlockState state = worldIn.getBlockState(pos);
+        Block block = state.getBlock();
+        if (state.isSideSolid(worldIn, pos, EnumFacing.DOWN) || state.getBlockFaceShape(worldIn, pos, EnumFacing.DOWN) == BlockFaceShape.SOLID)
+        {
+            return block != Blocks.END_GATEWAY;
+        }
+        else
+        {
+            return block instanceof BlockHangingFirecracker || block instanceof BlockFence || block instanceof BlockGlass || block instanceof BlockWall || block instanceof BlockStainedGlass;
+        }
     }
 
     @Override
@@ -60,6 +94,15 @@ public class BlockHangingFirecracker extends Block {
     public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
         if (!worldIn.isRemote && entityIn.isBurning()) {
             SpringFestivalUtil.createNonDestructiveExplosion(worldIn, pos, 3.0F, entityIn);
+        }
+    }
+    
+    @Override
+    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos)
+    {
+        if (!this.canBlockStay(worldIn, pos))
+        {
+            worldIn.destroyBlock(pos, true);
         }
     }
 }
