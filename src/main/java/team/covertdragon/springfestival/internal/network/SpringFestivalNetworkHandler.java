448997a3dd2d91ac55ea4b418f104df0496e9d7a
3TUSK <urey.s.knowledge@gmail.com>
2018-02-11 20:47:18 -0800

    Update copyright notice

--- src/main/java/team/covertdragon/springfestival/internal/network/SpringFestivalNetworkHandler.java
+++ src/main/java/team/covertdragon/springfestival/internal/network/SpringFestivalNetworkHandler.java
@@ -1,10 +1,12 @@
 /*
  * Copyright (c) 2018 CovertDragon Team.
+ * Copyright (c) 2018 Contributors of SpringFestival.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 /*
  * This file is adapted from FrogCraft: Rebirth, revision e0e04b12d2f8ded8eb54314b5a9d8582cfdfa89b
  *
@@ -14,27 +16,7 @@
  * FrogCraft: Rebirth is licensed under MIT. The original license header
  * are provided below.
  */
-/*
- * Copyright (c) 2015 - 2018 3TUSK, et al.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+
 
 package team.covertdragon.springfestival.internal.network;
 
--- src/main/java/team/covertdragon/springfestival/internal/time/ISpringFestivalTimeProvider.java
+++ src/main/java/team/covertdragon/springfestival/internal/time/ISpringFestivalTimeProvider.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018 CovertDragon Team.
+ * Copyright (c) 2018 Contributors of SpringFestival.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
