4d9331c08351c6ec8c684f8a9dbd0c1846037a4b
Snownee <1850986885@qq.com>
2018-02-13 11:08:47 +0800

    Working on firecracker

--- src/main/java/team/covertdragon/springfestival/internal/SpringFestivalUtil.java
+++ src/main/java/team/covertdragon/springfestival/internal/SpringFestivalUtil.java
@@ -9,19 +9,19 @@
 
 package team.covertdragon.springfestival.internal;
 
-import net.minecraft.entity.Entity;
+import java.util.Collections;
+
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeEventFactory;
 
-import java.util.Collections;
-
 public final class SpringFestivalUtil {
 
     private SpringFestivalUtil() {}
 
-    public static void createNonDestructiveExplosion(World world, BlockPos pos, float power, Entity source) {
+    public static void createNonDestructiveExplosion(World world, BlockPos pos, float power, EntityLivingBase source) {
         Explosion explosion = new Explosion(world, source, pos.getX(), pos.getY(), pos.getZ(), power, true, false, Collections.emptyList());
         // TODO Where is my sound effect
         if (ForgeEventFactory.onExplosionStart(world, explosion)) {
--- src/main/java/team/covertdragon/springfestival/module/firecracker/BlockHangingFirecracker.java
+++ src/main/java/team/covertdragon/springfestival/module/firecracker/BlockHangingFirecracker.java
@@ -21,17 +21,29 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import team.covertdragon.springfestival.internal.SpringFestivalUtil;
 
 @SuppressWarnings("deprecation")
 public class BlockHangingFirecracker extends Block {
+    
+    protected static final AxisAlignedBB AABB = new AxisAlignedBB(0.125D, 0.0D, 0.125D, 0.875D, 1.0D, 0.875D);
 
     // TODO Can redstone signal affect this firecracker?
 
@@ -39,6 +51,11 @@ public class BlockHangingFirecracker extends Block {
         super(Material.TNT, MapColor.RED);
     }
 
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return AABB;
+    }
+    
     @Nullable
     @Override
     public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
@@ -58,6 +75,49 @@ public class BlockHangingFirecracker extends Block {
     }
     
     @Override
+    public boolean isPassable(IBlockAccess worldIn, BlockPos pos)
+    {
+        return true;
+    }
+    
+    @Override
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        ItemStack itemstack = player.getHeldItem(hand);
+        
+        if (!itemstack.isEmpty() && (
+                itemstack.getItem() == Items.FLINT_AND_STEEL
+                ||
+                itemstack.getItem() == Items.FIRE_CHARGE
+                ||
+                itemstack.getItem() == Item.getItemFromBlock(Blocks.TORCH)
+                )
+            )
+        {
+            this.explode(world, pos, player);
+            
+            if (itemstack.getItem() == Items.FLINT_AND_STEEL)
+            {
+                itemstack.damageItem(1, player);
+            }
+            return true;
+        }
+        else
+        {
+            return super.onBlockActivated(world, pos, state, player, hand, facing, hitX, hitY, hitZ);
+        }
+    }
+    
+    public void explode(World worldIn, BlockPos pos, EntityLivingBase igniter)
+    {
+        if (!worldIn.isRemote)
+        {
+            SpringFestivalUtil.createNonDestructiveExplosion(worldIn, pos, 3.0F, igniter);
+            worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 11);
+        }
+    }
+
+    @Override
     public boolean canPlaceBlockAt(World worldIn, BlockPos pos)
     {
         return super.canPlaceBlockAt(worldIn, pos) ? this.canBlockStay(worldIn, pos) : false;
@@ -74,7 +134,7 @@ public class BlockHangingFirecracker extends Block {
         }
         else
         {
-            return block instanceof BlockHangingFirecracker || block instanceof BlockFence || block instanceof BlockGlass || block instanceof BlockWall || block instanceof BlockStainedGlass;
+            return block.isLeaves(state, worldIn, pos) || block instanceof BlockHangingFirecracker || block instanceof BlockFence || block instanceof BlockGlass || block instanceof BlockWall || block instanceof BlockStainedGlass;
         }
     }
 
@@ -86,14 +146,24 @@ public class BlockHangingFirecracker extends Block {
     @Override
     public void onBlockDestroyedByExplosion(World worldIn, BlockPos pos, Explosion explosionIn) {
         if (!worldIn.isRemote) {
-            SpringFestivalUtil.createNonDestructiveExplosion(worldIn, pos, 3.0F);
+            this.explode(worldIn, pos, explosionIn.getExplosivePlacedBy());
         }
     }
 
     @Override
-    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
-        if (!worldIn.isRemote && entityIn.isBurning()) {
-            SpringFestivalUtil.createNonDestructiveExplosion(worldIn, pos, 3.0F, entityIn);
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
+    {
+        if (!worldIn.isRemote && entityIn.isBurning())
+        {
+            if (entityIn instanceof EntityArrow)
+            {
+                EntityArrow entityarrow = (EntityArrow)entityIn;
+                this.explode(worldIn, pos, entityarrow.shootingEntity instanceof EntityLivingBase ? (EntityLivingBase)entityarrow.shootingEntity : null);
+            }
+            else if (entityIn instanceof EntityLivingBase)
+            {
+                this.explode(worldIn, pos, (EntityLivingBase)entityIn);
+            }
         }
     }
     
@@ -106,4 +176,14 @@ public class BlockHangingFirecracker extends Block {
         }
     }
 
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
+    
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
+    {
+        return BlockFaceShape.UNDEFINED;
+    }
 }
