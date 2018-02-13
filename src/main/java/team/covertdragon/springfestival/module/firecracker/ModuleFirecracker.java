1a04a9c5231269a0442b449527645918bbe21b72
Snownee <1850986885@qq.com>
2018-02-13 19:19:29 +0800

    Temporary recipes

--- src/main/java/team/covertdragon/springfestival/module/firecracker/ModuleFirecracker.java
+++ src/main/java/team/covertdragon/springfestival/module/firecracker/ModuleFirecracker.java
@@ -87,7 +87,7 @@ public class ModuleFirecracker extends AbstractSpringFestivalModule {
             IPosition iposition = BlockDispenser.getDispensePosition(source);
             EnumFacing enumfacing = (EnumFacing)source.getBlockState().getValue(BlockDispenser.FACING);
             IProjectile iprojectile = this.getProjectileEntity(world, iposition, stack);
-            iprojectile.shoot((double)enumfacing.getFrontOffsetX(), (double)((float)enumfacing.getFrontOffsetY() + 0.1F), (double)enumfacing.getFrontOffsetZ(), enumfacing != EnumFacing.UP ? 0.943F : 1.233F , 0.233F);
+            iprojectile.shoot((double)enumfacing.getFrontOffsetX(), (double)((float)enumfacing.getFrontOffsetY() + 0.1F), (double)enumfacing.getFrontOffsetZ(), enumfacing != EnumFacing.UP ? 0.943F : 1.2450F , 0.233F);
             world.spawnEntity((Entity)iprojectile);
             stack.shrink(1);
             return stack;
