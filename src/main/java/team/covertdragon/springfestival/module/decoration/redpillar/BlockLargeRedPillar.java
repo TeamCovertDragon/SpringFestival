ad73c6389c30839544bdd71e00d52eb05f6b02a9
SeraphJACK <seraphjack@outlook.com>
2018-02-14 11:28:50 +0800

    Textures from @SihenZhang

--- src/main/java/team/covertdragon/springfestival/module/decoration/redpillar/BlockLargeRedPillar.java
+++ src/main/java/team/covertdragon/springfestival/module/decoration/redpillar/BlockLargeRedPillar.java
@@ -1,13 +1,15 @@
 package team.covertdragon.springfestival.module.decoration.redpillar;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import team.covertdragon.springfestival.SpringFestivalConstants;
 
 public class BlockLargeRedPillar extends Block {
     public BlockLargeRedPillar() {
-        super(Material.WOOD, MapColor.RED);
+        super(Material.ROCK, MapColor.RED);
+        setSoundType(SoundType.STONE);
         setRegistryName(SpringFestivalConstants.MOD_ID, "large_red_pillar");
         setUnlocalizedName(SpringFestivalConstants.MOD_ID + ".large_red_pillar");
         setHardness(2.5F);
