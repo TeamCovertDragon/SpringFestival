e3249c9d5b868251f9a9a8fea8bcf5e154ac22d0
3TUSK <urey.s.knowledge@gmail.com>
2018-02-15 15:05:44 -0800

    Some crazy thingy

---
+++ src/main/java/team/covertdragon/springfestival/module/decoration/fudoor/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 CovertDragon Team.
+ * Copyright (c) 2018 Contributors of SpringFestival.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package team.covertdragon.springfestival.module.decoration.fudoor;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
