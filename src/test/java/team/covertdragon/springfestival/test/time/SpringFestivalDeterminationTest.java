5201adf096fe05bffa02138c5a95bc0e2e9979f6
3TUSK <urey.s.knowledge@gmail.com>
2018-03-01 00:49:48 -0800

    Close #14

---
+++ src/test/java/team/covertdragon/springfestival/test/time/SpringFestivalDeterminationTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018 CovertDragon Team.
+ * Copyright (c) 2018 Contributors of SpringFestival.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package team.covertdragon.springfestival.test.time;
+
+import org.junit.Test;
+
+public class SpringFestivalDeterminationTest {
+
+    @Test
+    public void testSpringFestivalChecking() {
+
+    }
+}
