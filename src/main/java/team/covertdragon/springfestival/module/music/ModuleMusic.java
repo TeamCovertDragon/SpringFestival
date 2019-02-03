aa0953de6b945f3bed07b4ad863a6100a891e7dc
3TUSK <urey.s.knowledge@gmail.com>
2019-02-03 15:22:58 -0800

    Reserve our first sound track. We actually need a composer now...

--- src/main/java/team/covertdragon/springfestival/module/music/ModuleMusic.java
+++ src/main/java/team/covertdragon/springfestival/module/music/ModuleMusic.java
@@ -9,18 +9,39 @@
 
 package team.covertdragon.springfestival.module.music;
 
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import team.covertdragon.springfestival.SpringFestivalConstants;
 import team.covertdragon.springfestival.module.AbstractSpringFestivalModule;
+import team.covertdragon.springfestival.module.SpringFestivalModule;
 
+@SpringFestivalModule(name = "music")
 public class ModuleMusic extends AbstractSpringFestivalModule {
 
-    // TODO sound.json, actual music & their discs
+    @GameRegistry.ObjectHolder("springfestival:track_01")
+    public static SoundEvent SPRING_FESTIVAL_TRACK_01;
 
+    @SubscribeEvent
+    public void onItemRegistry(RegistryEvent.Register<Item> event) {
+        if (SPRING_FESTIVAL_TRACK_01 == null) {
+            SPRING_FESTIVAL_TRACK_01 = new SoundEvent(new ResourceLocation(SpringFestivalConstants.MOD_ID, "track.01"));
+        }
+        event.getRegistry().register(
+                new ItemSpringFestivalMusicRecord("track.01", SPRING_FESTIVAL_TRACK_01)
+                        .setCreativeTab(SpringFestivalConstants.CREATIVE_TAB)
+                        .setRegistryName(SpringFestivalConstants.MOD_ID, "track_01")
+        );
+    }
 
     @SubscribeEvent
     public void onSoundEventRegistry(RegistryEvent.Register<SoundEvent> event) {
-
+        if (SPRING_FESTIVAL_TRACK_01 == null) {
+            return;
+        }
+        event.getRegistry().register(SPRING_FESTIVAL_TRACK_01.setRegistryName(SpringFestivalConstants.MOD_ID, "track_01"));
     }
 }
